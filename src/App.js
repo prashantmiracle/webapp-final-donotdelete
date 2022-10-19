be2561a02cbcb3db4aa2117b385ec0453a5b6eaa
prashant <prashantmiracle@gmail.com>
2022-10-19 21:46:07 +0530

    first

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>    
+          <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
         </p>
         <a
           className="App-link"
